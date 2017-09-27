8f8bf2e938ed27a9962c53bb2dd11a7f9d585cff
nimf8 <nimf.87@mail.ru>
2017-09-27 21:34:18 +0300

    zxfds

--- moduleJar/src/main/java/figure/Figure.java
+++ moduleJar/src/main/java/figure/Figure.java
@@ -16,7 +16,7 @@ public class Figure {
                 * ((-lengthA + lengthB + lengthC) / 2)
                 * ((lengthA - lengthB + lengthC) / 2)
                 * ((lengthA + lengthB - lengthC) / 2);
-    }
+    }//
 
     public double getPerimeterTriangle(double lengthA, double lengthB, double lengthC) {
         return lengthA + lengthB + lengthC;
