9d4664b26277d2d10995d8fa41358e8a051f12e0
art_gluhovskiy <gluhovskiy_art@mail.ru>
2017-09-27 20:57:54 +0300

    add

---
+++ moduleJar/src/main/java/figure/Figure.java
@@ -0,0 +1,12 @@
+package figure;
+
+public class Figure {
+
+    public double getRectArea(int a, int b) {
+        return a * b;
+    }
+
+    public double getRectPerim(int a, int b) {
+        return 2 * a + 2 * b;
+    }
+}
