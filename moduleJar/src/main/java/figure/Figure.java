3a2016159eb3d98afc46e6bb46fcdf80bc190636
nimf8 <nimf.87@mail.ru>
2017-09-27 20:54:57 +0300

    zxfds

---
+++ moduleJar/src/main/java/figure/Figure.java
@@ -0,0 +1,11 @@
+package figure;
+
+public class Figure {
+
+    public double getCircleArea(double r){
+        return Math.PI*r;
+    }
+    public double getCirclePerim(double r){
+        return r*2*Math.PI;
+    }
+}
