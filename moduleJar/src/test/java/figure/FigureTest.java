24c324d2eec598e5621cb45e2be25cc424ad4d22
nimf8 <nimf.87@mail.ru>
2017-09-28 13:05:22 +0300

    add new Test

--- moduleJar/src/test/java/figure/FigureTest.java
+++ moduleJar/src/test/java/figure/FigureTest.java
@@ -1,4 +1,20 @@
 package figure;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FigureTest {
+    Figure circle = new Figure();
+    double r = 5;
+    @Test
+    public void getCircleAreaTest(){
+        double area = circle.getCircleArea(r);
+        assertEquals(area, Math.PI*r);
+    }
+    @Test
+    public void getCirclePerimTest(){
+        double perim = circle.getCirclePerim(r);
+        assertEquals(perim, Math.PI*r*2);
+    }
 }
